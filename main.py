c24a53cf82f2db4d25096bb2a7b93c9d92a35179
Kyle Misteld <kmistele@smu.edu>
2020-04-26 22:29:01 -0500

    Added corpus text to search

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+# IMPORTS
+
+# MAIN
+def main():
+    print("Hello world!")
+
+    corpus = ""
+    with open("corpus.txt", 'r') as f:
+        corpus = f.read()
+
+    print(f'Length of corpus: {len(corpus)} characters')
+
+main()
