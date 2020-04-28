85c3dcdc5dc380ebb422fb3578443df2b892184e
Kyle Misteld <kmistele@smu.edu>
2020-04-28 14:23:43 -0500

    Added method to create the bad character table for Boyer Moore search algorithm

--- library/boyerMooreSearch.py
+++ library/boyerMooreSearch.py
@@ -1,5 +1,20 @@
-def _badCharacterTable(str):
-    print("Bad character table")
+# IMPORTS
+import string
+
+def _createBadCharacterTable(pattern):
+    print("Building bad character table")
+
+    # CREATE AN ALPHABET THAT IS ALL OF THE PRINTABLE STRING CHARACTERS
+    alphabet = str(string.printable)
+    alphabetSize = len(alphabet)
+
+    # CREATE THE TABLE (dictionary) THAT STORES THE INDEX OF THE LAST OCURRENCE OF THE CHARACTER IN THE STRING
+    table = {}
+    i = 0
+    for character in pattern:
+        table[character] = i
+        i += 1
+    print(table)
 
 def _goodSuffixHeuristic():
     print("Good suffic heuristic")
@@ -11,5 +26,9 @@ def boyerMooreSearch(corpus, searchTerm):
     # GRAB LENGTHS OF BOTH STRINGS FOR LOOP CONTROL
     corpusLength = len(corpus)
     searchTermLength = len(searchTerm)
+
+    # CREATE THE BAD CHARACTER TABLE
+    badCharTable = _createBadCharacterTable(searchTerm)
+    print(badCharTable)
 
     # TODO: FINISH ME
