78a324fff5d350c7bb7251008f1ac7854c2c4471
Kyle Misteld <kmistele@smu.edu>
2020-04-28 10:35:36 -0500

    Implemented naive string search

--- library/searchfunctions.py
+++ library/searchfunctions.py
@@ -1,6 +1,36 @@
 
 def naiveSearch(corpus, searchTerm):
     print(f'Searching corpus for {searchTerm}')
+    corpusLength = len(corpus)
+    searchTermLength = len(searchTerm)
+
+    # CREATE ALIGNMENTS
+    for i in range(0, corpusLength - searchTermLength + 1):
+        # CREATE A POINTER FOR ALIGNMENTS. IT STARTS AT THE BEGINNING OF THE PATTERN, I.E. AT INDEX 0
+        # IF THE FIRST CHARACTER OF THE ALIGNMENT MATCHES, KEEP INCREMENTING IT
+        alignmentPointer = 0
+
+        # USE THE ALIGNMENT POINTER TO CHECK THE ALIGNMENT
+        alignmentFound = True
+
+        while alignmentPointer < searchTermLength:
+            # IF THE CHARACTER MATCHES, CHECK THE NEXT ONE IN THE ALIGNMENT
+            if searchTerm[alignmentPointer] == corpus[i + alignmentPointer]:
+                alignmentPointer += 1
+
+            # OTHERWISE, BREAK AND MOVE TO THE NEXT ALIGNMENT
+            else:
+                alignmentFound = False
+                break
+
+        # IF AN ALIGNMENT WAS NOT FOUND, MOVE TO THE NEXT ALIGNMENT
+        if not alignmentFound:
+            continue
+
+        # IF AN ALIGNENT WAS FOUND, PRINT IT!
+        else:
+            print(f'Found an alignment at {i}!')
+
 
 def boyerMooreSearch(corpus, searchTerm):
     print(f'Using Boyer-Moore to search corpus for {searchTerm}')
